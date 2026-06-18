57ca38af8956986506da8247a18e41fde8b331a8
Charagondla Varshini <varshinicharagondla0706@gmail.com>
2026-06-18 21:05:19 +0530

    Add LeetCode 167 Two Sum II solution in Java

---
+++ Medium/167-Two-Sum-II.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int left=0,right=nums.length-1;
+        while(left<right){
+            if(nums[left]+nums[right]==target){
+                return new int[]{left+1,right+1};
+            }
+            if(nums[left]+nums[right]<target){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return new int[]{left+1,right+1};
+    }
+}
