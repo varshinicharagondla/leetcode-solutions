38bb654d0806595a0d95429653cb72f372e220fe
Charagondla Varshini <varshinicharagondla0706@gmail.com>
2026-07-01 20:17:58 +0530

    Add LeetCode 2520 Count the Digits That Divide a Number solution

---
+++ Easy/2520-Count The Digits That Divide a Number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countDigits(int num) {
+        int count=0;
+        int temp=num;
+        while(num>0){
+            int ld=num%10;
+            if(temp%ld==0){
+                count++;
+            }
+            num/=10;
+
+        }
+        return count;
+    }
+}
